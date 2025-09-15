318dfe6a6edbd71eb4af199b12cb3969c7dd43bc
DamaMichaelYohanna <get2dama11@gmail.com>
2025-09-15 22:59:36 +0100

    new update

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from main.views import index_view, login
+from main.views import index_view, login, about_us, contact_us
 
 urlpatterns = [
     path('', index_view, name='index'),
     path('login',  login, name='login_page'),
+    path('about',  about_us, name='about_us_page'),
+    path('contact',  contact_us, name='contact_us_page')
 
 ]
 
--- main/views.py
+++ main/views.py
@@ -16,3 +16,10 @@ def login(request):
         pass
 
     return render(request, 'login.html')
+
+
+def about_us(request):
+    return render(request, 'about_us.html')
+
+def contact_us(request):
+    return render(request, 'contact_us.html')
