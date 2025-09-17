1e0cd6001ea6e9cb5392ccd8d4260d97cae97f12
DamaMichaelYohanna <get2dama11@gmail.com>
2025-09-17 12:06:23 +0100

    update our project

--- main/urls.py
+++ main/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from main.views import index_view, login, about_us, contact_us
+from main.views import index_view, login_view, about_us, contact_us, register_view
 
 urlpatterns = [
     path('', index_view, name='index'),
-    path('login',  login, name='login_page'),
+    path('login',  login_view, name='login'),
     path('about',  about_us, name='about_us_page'),
-    path('contact',  contact_us, name='contact_us_page')
+    path('contact',  contact_us, name='contact_us_page'),
+    path('register',  register_view, name='register'),
 
 ]
 
--- main/views.py
+++ main/views.py
@@ -1,22 +1,40 @@
 from django.shortcuts import render, redirect
-
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
 
 def index_view(request):
     return render(request, 'index.html')
 
 
-def login(request):
+def login_view(request):
     if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        print("The username is", username)
-        print("The password is", password)
-        return redirect("/")
-    else:
-        pass
-
+        username_value = request.POST['username']
+        password_value = request.POST['password']
+        user = authenticate(request,
+                             username=username_value, 
+                             password=password_value)
+        if user is not None:
+            login(request, user)
+            return redirect("/")
+        else:
+            return render(request, 'login.html', {'error': 'Invalid username or password'})
+  
     return render(request, 'login.html')
 
+def register_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+        if password1 != password2:
+            return render(request, 'register.html', {'error': 'Passwords do not match'})
+        user = User.objects.create_user(username=username, email=email, password=password1)
+        user.first_name = 'Test First Name'
+        user.last_name = 'Test Last Name'
+        user.save()
+        return redirect('/login')
+    return render(request, 'register.html')
 
 def about_us(request):
     return render(request, 'about_us.html')
